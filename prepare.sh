a7f0b47bd5bda51adb30fdcc594c5a8a057db155
Bernard Xiong <bernard.xiong@gmail.com>
2015-03-13 09:31:06 +0000

    Add sudo for scons installation

--- prepare.sh
+++ prepare.sh
@@ -2,8 +2,9 @@
 git clone https://github.com/RT-Thread/rt-thread.git
 
 # prepare toolchains and scons 
-apt-get install scons
+sudo apt-get install scons
 
+# download toolchain
 if [ ! -f "arm-2012.09-63-arm-none-eabi-i686-pc-linux-gnu.tar.bz2" ];then
 wget -c https://sourcery.mentor.com/public/gnu_toolchain/arm-none-eabi/arm-2012.09-63-arm-none-eabi-i686-pc-linux-gnu.tar.bz2
 tar jxvf arm-2012.09-63-arm-none-eabi-i686-pc-linux-gnu.tar.bz2
