f079259d1415ecbcb2e83f9c9ee02105741363c7
Bernard Xiong <bernard.xiong@gmail.com>
2015-03-13 17:25:56 +0800

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,5 @@
+# toolchains script
+export PATH=/home/ubuntu/workspace/arm-2012.09/bin:$PATH
+export RTT_ROOT=/home/ubuntu/workspace/rt-thread
+export RTT_CC=gcc
+export RTT_EXEC_PATH=/home/ubuntu/workspace/arm-2012.09/bin
